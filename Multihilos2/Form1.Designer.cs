e3452caff39438e72403f15fc34fbf2f43b21376
Andrea Evelyn Mejía Rubio <andiemartin33@gmail.com>
2021-03-22 10:02:56 -0600

    Agregar archivos de proyecto.

---
+++ Multihilos2/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Multihilos2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnprocesos = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(315, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hilos";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(41, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(152, 207);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(258, 94);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(152, 207);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(482, 94);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(152, 207);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnprocesos
+            // 
+            this.btnprocesos.Location = new System.Drawing.Point(196, 331);
+            this.btnprocesos.Name = "btnprocesos";
+            this.btnprocesos.Size = new System.Drawing.Size(75, 23);
+            this.btnprocesos.TabIndex = 4;
+            this.btnprocesos.Text = "Procesos";
+            this.btnprocesos.UseVisualStyleBackColor = true;
+            this.btnprocesos.Click += new System.EventHandler(this.btnprocesos_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Location = new System.Drawing.Point(364, 331);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(75, 23);
+            this.btnsalir.TabIndex = 5;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 397);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btnprocesos);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnprocesos;
+        private System.Windows.Forms.Button btnsalir;
+    }
+}
+
---
+++ Multihilos2/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Multihilos2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+            this.Close();
+        }
+
+        private void btnprocesos_Click(object sender, EventArgs e)
+        {
+            //creacion de hilos
+            Thread h1 = new Thread(new ThreadStart(Metodo1));
+            Thread h2 = new Thread(new ThreadStart(Metodo2));
+            Thread h3 = new Thread(new ThreadStart(Metodo3));
+
+
+            //inicializacion de hilos
+            h1.Start();
+            h2.Start();
+            h3.Start();
+
+
+            //cierre de hilo
+            h1.Join();
+            h2.Join();
+            h3.Join();
+
+        }
+
+        public void Metodo1()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                this.pictureBox1.Load("i1.JPG");
+                Thread.Sleep(300);
+                this.pictureBox1.Load("i2.JPG");
+                Thread.Sleep(300);
+                this.pictureBox1.Load("i3.JPG");
+                Thread.Sleep(300);
+                this.pictureBox1.Load("i4.JPG");
+                Thread.Sleep(300);
+                this.pictureBox1.Load("i5.JPG");
+                Thread.Sleep(300);
+            }
+
+
+        }
+
+        public void Metodo2()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                this.pictureBox2.Load("i6.JPG");
+                Thread.Sleep(300);
+                this.pictureBox2.Load("i7.JPG");
+                Thread.Sleep(300);
+                this.pictureBox2.Load("i8.JPG");
+                Thread.Sleep(300);
+                this.pictureBox2.Load("i9.JPG");
+                Thread.Sleep(300);
+                this.pictureBox2.Load("i10.JPG");
+                Thread.Sleep(300);
+            }
+
+
+        }
+
+        public void Metodo3()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                this.pictureBox3.Load("i11.JPG");
+                Thread.Sleep(300);
+                this.pictureBox3.Load("i12.JPG");
+                Thread.Sleep(300);
+                this.pictureBox3.Load("i13.JPG");
+                Thread.Sleep(300);
+                this.pictureBox3.Load("i14.JPG");
+                Thread.Sleep(300);
+                this.pictureBox3.Load("i15.JPG");
+                Thread.Sleep(300);
+            }
+
+        }
+
+
+
+    }
+}
